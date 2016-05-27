cb44d372c5820ffba3c5a01bf07cd306b73422da
Carter Shanklin <carter@HW11972.local>
2016-05-26 18:20:49 -0700

    Added a less trivial example of using PQS to page through the system catalog.

--- modules/odbc_phoenix/files/samplePythonODBCQuery.py
+++ modules/odbc_phoenix/files/samplePythonODBCQuery.py
@@ -1,10 +1,22 @@
 import pyodbc
+import time
 
 cnxn = pyodbc.connect('DSN=Hortonworks Phoenix ODBC DSN', autocommit=True)
 cursor = cnxn.cursor()
 
-print "Number of records in catalog"
-cursor.execute("select count(*) from system.catalog")
-row = cursor.fetchone()
-if row:
-	print row
+print "Paging through the system catalog"
+offset = 0
+rows = 10
+while rows == 10:
+	statement = "select * from system.catalog offset {0} fetch first 10 rows only".format(offset)
+	print "Selecting catalog rows starting from offset", offset
+	print statement
+	time.sleep(2)
+	cursor.execute(statement)
+	rows = 0
+	row = cursor.fetchone()
+	while row:
+		print row[1:4]
+		row = cursor.fetchone()
+		rows += 1
+	offset += 10
