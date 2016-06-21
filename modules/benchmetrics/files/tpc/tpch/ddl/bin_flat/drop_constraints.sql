8fc612b1394246408a175785c4cc3592e3c20573
Carter Shanklin <carter@HW11972.local>
2016-06-21 13:36:09 -0700

    PK / FK constraints

---
+++ modules/benchmetrics/files/tpc/tpch/ddl/bin_flat/drop_constraints.sql
@@ -0,0 +1,16 @@
+alter table region drop constraint region_c1;
+alter table nation drop constraint nation_c1;
+alter table nation drop constraint nation_c2;
+alter table part drop constraint part_c1;
+alter table supplier drop constraint supplier_c1;
+alter table supplier drop constraint supplier_c2;
+alter table partsupp drop constraint partsupp_c1;
+alter table customer drop constraint customer_c1;
+alter table customer drop constraint customer_c2;
+alter table lineitem drop constraint lineitem_c1;
+alter table orders drop constraint orders_c1;
+alter table partsupp drop constraint partsupp_c2;
+alter table partsupp drop constraint partsupp_c3;
+alter table orders drop constraint orders_c2;
+alter table lineitem drop constraint lineitem_c2;
+alter table lineitem drop constraint lineitem_c3;
