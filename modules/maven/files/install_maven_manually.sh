ce88b01bb855046ad51794a49247303e3ce11160
Carter Shanklin <carter@HW11972.local>
2016-09-11 14:49:11 -0700

    Added an option for a manual Maven install

---
+++ modules/maven/files/install_maven_manually.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+maven_version="3.3.9"
+maven_base="apache-maven-$maven_version"
+maven_bin="$maven_base-bin.tar.gz"
+maven_dist="http://mirrors.ibiblio.org/apache/maven/maven-3/$maven_version/binaries/$maven_bin"
+m2_home="/usr/local/share/$maven_base"
+cd /tmp
+curl -C - -O $maven_dist
+sudo tar -C /usr/local/share -xvf $maven_bin
+sudo ln -s /usr/local/share/$maven_base /usr/local/share/maven
+sudo cp /vagrant/modules/maven/files/maven.sh /etc/profile.d/maven.sh
