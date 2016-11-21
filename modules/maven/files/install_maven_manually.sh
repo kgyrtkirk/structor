51cfa423bee58904b70ad914b6f9763ce998ba58
Carter Shanklin <carter@HW11972.local>
2016-11-21 10:21:02 -0800

    Copy in some cached JARs if they exist

--- modules/maven/files/install_maven_manually.sh
+++ modules/maven/files/install_maven_manually.sh
@@ -10,3 +10,5 @@ curl -C - -O $maven_dist
 sudo tar -C /usr/local/share -xvf $maven_bin
 sudo ln -s /usr/local/share/$maven_base /usr/local/share/maven
 sudo cp /vagrant/modules/maven/files/maven.sh /etc/profile.d/maven.sh
+cp -r /vagrant/.m2 /home/vagrant
+chmod -R 775 /home/vagrant/.m2
