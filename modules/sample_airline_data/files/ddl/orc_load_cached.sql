487effd80d86979f3054bb0d61afacb75c2eef70
Carter Shanklin <carter@HW11972.local>
2016-11-27 17:21:28 -0800

    Use dates and boolean types where applicable.

--- modules/sample_airline_data/files/ddl/orc_load_cached.sql
+++ modules/sample_airline_data/files/ddl/orc_load_cached.sql
@@ -7,78 +7,42 @@ drop table if exists airlines;
 drop table if exists planes;
 
 create table airports (
-	iata string,
-	airport string,
-	city string,
-	state string,
-	country string,
-	lat double,
-	lon double
+    iata string, airport string, city string,
+    state string, country string, lat double,
+    lon double
 )
 STORED AS ORC
-location '/apps/hive/warehouse/airline_ontime.db/airports'
-;
+location '/apps/hive/warehouse/airline_ontime.db/airports';
 
 create table airlines (
-	code string,
-	description string
+    code string, description string
 )
 STORED AS ORC
-location '/apps/hive/warehouse/airline_ontime.db/airlines'
-;
+location '/apps/hive/warehouse/airline_ontime.db/airlines';
 
 create table planes (
-	tailnum string,
-	owner_type string,
-	manufacturer string,
-	issue_date string,
-	model string,
-	status string,
-	aircraft_type string,
-	engine_type string,
-	year int
+    tailnum string, owner_type string, manufacturer string,
+    issue_date string, model string, status string,
+    aircraft_type string, engine_type string, year int
 )
 STORED AS ORC
-location '/apps/hive/warehouse/airline_ontime.db/planes'
-;
+location '/apps/hive/warehouse/airline_ontime.db/planes';
 
 create table flights (
-  Month int,
-  DayofMonth int,
-  DayOfWeek int,
-  DepTime  int,
-  CRSDepTime int,
-  ArrTime int,
-  CRSArrTime int,
-  UniqueCarrier varchar(6),
-  FlightNum int,
-  TailNum varchar(8),
-  ActualElapsedTime int,
-  CRSElapsedTime int,
-  AirTime int,
-  ArrDelay int,
-  DepDelay int,
-  Origin varchar(3),
-  Dest varchar(3),
-  Distance int,
-  TaxiIn int,
-  TaxiOut int,
-  Cancelled int,
-  CancellationCode varchar(1),
-  Diverted varchar(1),
-  CarrierDelay int,
-  WeatherDelay int,
-  NASDelay int,
-  SecurityDelay int,
-  LateAircraftDelay int
-) 
+  FlightDate date, DepTime int, CRSDepTime int,
+  ArrTime int, CRSArrTime int, UniqueCarrier string,
+  FlightNum int, TailNum string, ActualElapsedTime int,
+  CRSElapsedTime int, AirTime int, ArrDelay int,
+  DepDelay int, Origin string, Dest string,
+  Distance int, TaxiIn int, TaxiOut int,
+  Cancelled int, CancellationCode string, Diverted string,
+  CarrierDelay int, WeatherDelay int, NASDelay int,
+  SecurityDelay int, LateAircraftDelay int
+)
 PARTITIONED BY (Year int)
-CLUSTERED BY (Month)
-SORTED BY (DayofMonth) into 12 buckets
 STORED AS ORC
 location '/apps/hive/warehouse/airline_ontime.db/flights'
-TBLPROPERTIES("orc.bloom.filter.columns"="*")
-;
+TBLPROPERTIES("orc.bloom.filter.columns"="*");
 
 alter table flights add partition (year=1987);
 alter table flights add partition (year=1988);
