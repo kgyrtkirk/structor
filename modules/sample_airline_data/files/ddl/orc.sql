77e56afce58a5b6ed7cc8596e11cf35be555d6cc
Carter Shanklin <carter@HW11972.local>
2016-11-27 17:21:06 -0800

    Use date and boolean types where appropriate

--- modules/sample_airline_data/files/ddl/orc.sql
+++ modules/sample_airline_data/files/ddl/orc.sql
@@ -10,108 +10,58 @@ drop table if exists airlines purge;
 drop table if exists planes purge;
 
 create table airports (
-	iata string,
-	airport string,
-	city string,
-	state string,
-	country string,
-	lat double,
-	lon double
-)
-STORED AS ORC
-;
+    iata string, airport string, city string,
+    state string, country string, lat double, lon double
+) STORED AS ORC;
+insert overwrite table airports select * from airports_raw;
 
 create table airlines (
-	code string,
-	description string
-)
-STORED AS ORC
-;
+    code string, description string
+) STORED AS ORC;
+insert overwrite table airlines select * from airlines_raw;
 
 create table planes (
-	tailnum string,
-	owner_type string,
-	manufacturer string,
-	issue_date string,
-	model string,
-	status string,
-	aircraft_type string,
-	engine_type string,
-	year int
-)
-STORED AS ORC
-;
+    tailnum string, owner_type string,
+    manufacturer string, issue_date date,
+    model string, status string,
+    aircraft_type string, engine_type string, year int
+) STORED AS ORC;
 
-insert overwrite table airports select * from airports_raw;
-insert overwrite table airlines select * from airlines_raw;
-insert overwrite table planes select * from planes_raw;
+insert overwrite table planes select
+    tailnum, owner_type, manufacturer,
+    cast (concat(substr(issue_date, 7, 4), "-", substr(issue_date, 4, 2), "-", substr(issue_date, 1, 2)) as date),
+    model, status, aircraft_type, engine_type, year
+from planes_raw;
 
 create table flights (
-  Month int,
-  DayofMonth int,
-  DayOfWeek int,
-  DepTime  int,
-  CRSDepTime int,
-  ArrTime int,
-  CRSArrTime int,
-  UniqueCarrier varchar(6),
-  FlightNum int,
-  TailNum varchar(8),
-  ActualElapsedTime int,
-  CRSElapsedTime int,
-  AirTime int,
-  ArrDelay int,
-  DepDelay int,
-  Origin varchar(3),
-  Dest varchar(3),
-  Distance int,
-  TaxiIn int,
-  TaxiOut int,
-  Cancelled int,
-  CancellationCode varchar(1),
-  Diverted varchar(1),
-  CarrierDelay int,
-  WeatherDelay int,
-  NASDelay int,
-  SecurityDelay int,
-  LateAircraftDelay int
+  FlightDate date, DepTime int, CRSDepTime int,
+  ArrTime int, CRSArrTime int, UniqueCarrier string,
+  FlightNum int, TailNum string, ActualElapsedTime int,
+  CRSElapsedTime int, AirTime int, ArrDelay int,
+  DepDelay int, Origin string, Dest string,
+  Distance int, TaxiIn int, TaxiOut int,
+  Cancelled boolean, CancellationCode string, Diverted boolean,
+  CarrierDelay int, WeatherDelay int, NASDelay int,
+  SecurityDelay int, LateAircraftDelay int
 ) 
 PARTITIONED BY (Year int)
-CLUSTERED BY (Month)
-SORTED BY (DayofMonth) into 12 buckets
 STORED AS ORC
-TBLPROPERTIES("orc.bloom.filter.columns"="*")
-;
+TBLPROPERTIES("orc.bloom.filter.columns"="*");
 
 insert overwrite table flights partition(year) 
 select
-  Month,
-  DayofMonth,
-  DayOfWeek,
-  DepTime ,
-  CRSDepTime,
-  ArrTime,
-  CRSArrTime,
-  UniqueCarrier,
-  FlightNum,
-  TailNum,
-  ActualElapsedTime,
-  CRSElapsedTime,
-  AirTime,
-  ArrDelay,
-  DepDelay,
-  Origin,
-  Dest,
-  Distance,
-  TaxiIn,
-  TaxiOut,
-  Cancelled,
+  cast(concat(year, "-", month, "-", dayofmonth) as date) as FlightDate,
+  DepTime, CRSDepTime, ArrTime,
+  CRSArrTime, UniqueCarrier, FlightNum,
+  TailNum, ActualElapsedTime, CRSElapsedTime,
+  AirTime, ArrDelay, DepDelay,
+  Origin, Dest, Distance,
+  TaxiIn, TaxiOut,
+  case when Cancelled = 0 then false else true end,
   CancellationCode,
-  Diverted,
+  case when Diverted = 0 then false else true end,
   CarrierDelay,
-  WeatherDelay,
-  NASDelay,
-  SecurityDelay,
-  LateAircraftDelay,
-  Year
-  from flights_raw;
+  WeatherDelay, NASDelay, SecurityDelay,
+  LateAircraftDelay, Year
+  from flights_raw
+order by FlightDate;
