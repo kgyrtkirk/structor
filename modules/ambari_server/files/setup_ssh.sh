f497d21db5bb50a3c59111a223c03fe1573c1469
Carter Shanklin <carter@HW11972.local>
2017-03-03 11:00:12 -0800

    Sudoize the SSH setup

--- modules/ambari_server/files/setup_ssh.sh
+++ modules/ambari_server/files/setup_ssh.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-mkdir -p /root/.ssh
-chmod 700 /root/.ssh
-ssh-keygen -f /root/.ssh/id_rsa -q -N ""
-cp /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys
-ssh-keyscan -H $(hostname) >> /root/.ssh/known_hosts
-ssh-keyscan -H $(hostname -a) >> /root/.ssh/known_hosts
-ssh-keyscan -H localhost >> /root/.ssh/known_hosts
+sudo mkdir -p /root/.ssh
+sudo chmod 700 /root/.ssh
+sudo ssh-keygen -f /root/.ssh/id_rsa -q -N ""
+sudo cp /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys
+sudo ssh-keyscan -H $(hostname) >> /root/.ssh/known_hosts
+sudo ssh-keyscan -H $(hostname -a) >> /root/.ssh/known_hosts
+sudo ssh-keyscan -H localhost >> /root/.ssh/known_hosts
