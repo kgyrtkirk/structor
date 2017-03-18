77edf8cbaa8738321c7d19425d043b76d9c7f483
Carter Shanklin <carter@HW11972.local>
2017-03-18 09:37:42 -0700

    Converting some tabs to spaces

--- modules/benchmetrics/files/tpc/tpch/queries/tpch_query4.sql
+++ modules/benchmetrics/files/tpc/tpch/queries/tpch_query4.sql
@@ -1,23 +1,23 @@
 use ${DB};
 
 select
-	o_orderpriority,
-	count(*) as order_count
+    o_orderpriority,
+    count(*) as order_count
 from
-	orders as o
+    orders as o
 where
-	o_orderdate >= '1996-05-01'
-	and o_orderdate < '1996-08-01'
-	and exists (
-		select
-			*
-		from
-			lineitem
-		where
-			l_orderkey = o.o_orderkey
-			and l_commitdate < l_receiptdate
-	)
+    o_orderdate >= '1996-05-01'
+    and o_orderdate < '1996-08-01'
+    and exists (
+        select
+            *
+        from
+            lineitem
+        where
+            l_orderkey = o.o_orderkey
+            and l_commitdate < l_receiptdate
+    )
 group by
-	o_orderpriority
+    o_orderpriority
 order by
-	o_orderpriority;
+    o_orderpriority;
