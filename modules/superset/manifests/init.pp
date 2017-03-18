9d6755e4f1ce544fe8ce66d32ccc200d3c7ca8be
Carter Shanklin <carter@HW11972.local>
2017-03-18 09:45:11 -0700

    Superset

---
+++ modules/superset/manifests/init.pp
@@ -0,0 +1,69 @@
+#  Licensed to the Apache Software Foundation (ASF) under one or more
+#   contributor license agreements.  See the NOTICE file distributed with
+#   this work for additional information regarding copyright ownership.
+#   The ASF licenses this file to You under the Apache License, Version 2.0
+#   (the "License"); you may not use this file except in compliance with
+#   the License.  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+class druid_superset {
+  require druid_base
+
+  $path="/usr/hdp/${hdp_version}/superset/bin:/bin:/usr/bin:/usr/sbin"
+
+  # Install and setup.
+  package { "epel-release":
+    ensure => installed,
+  }
+  ->
+  package { "superset${package_version}":
+    ensure => installed,
+  }
+  ->
+  exec { "fabmanager create-admin --app superset --username admin --password admin --firstname admin --lastname user --email nobody@example.com":
+    cwd => "/",
+    path => "$path",
+  }
+  ->
+  exec { "superset db upgrade":
+    cwd => "/",
+    path => "$path",
+  }
+  ->
+  exec { "superset load_examples":
+    cwd => "/",
+    path => "$path",
+  }
+  ->
+  exec { "superset init":
+    cwd => "/",
+    path => "$path",
+  }
+  ->
+  file { "/usr/hdp/current/superset":
+    ensure => link,
+    target => "/usr/hdp/${hdp_version}/superset",
+    before => Service["superset"],
+  }
+
+  # Startup.
+  if ($operatingsystem == "centos" and $operatingsystemmajrelease == "7") {
+    file { "/etc/systemd/system/superset.service":
+      ensure => 'file',
+      source => "/vagrant/files/systemd/superset.service",
+      before => Service["superset"],
+    }
+  }
+
+  service { 'superset':
+    ensure => running,
+    enable => true,
+  }
+}
