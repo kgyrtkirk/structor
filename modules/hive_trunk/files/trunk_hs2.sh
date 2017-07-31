48fdf4409cff9d3d089ef08c7ec926c4297c1f48
Carter Shanklin <carter@HW11972.local>
2017-07-30 20:34:50 -0700

    Improvements for trunk integration

--- modules/hive_trunk/files/trunk_hs2.sh
+++ modules/hive_trunk/files/trunk_hs2.sh
@@ -4,9 +4,11 @@
 sudo systemctl stop hive-server2
 sudo systemctl stop hive2-server2
 sudo systemctl stop hive-metastore
+sudo systemctl stop hive2-metastore
 sudo systemctl disable hive-server2
 sudo systemctl disable hive2-server2
 sudo systemctl disable hive-metastore
+sudo systemctl disable hive2-metastore
 
 # Upgrade the schema.
 export HIVE_CONF_DIR=/home/vagrant/hivedist/hive/conf
@@ -15,14 +17,11 @@ cd ~/hivedist/hive
 bin/schematool -dbType mysql -upgradeSchema
 
 # Put new control scripts in place.
-sudo cp /vagrant/modules/hive_trunk/files/hive-metastore.service /etc/systemd/system
+sudo cp /vagrant/modules/hive_trunk/files/hive2-metastore.service /etc/systemd/system
 sudo cp /vagrant/modules/hive_trunk/files/hive2-server2.service /etc/systemd/system
-sudo mkdir -p /etc/systemd/system/hive2-server2.service.d
-sudo cp /vagrant/modules/hive_trunk/files/default.conf /etc/systemd/system/hive2-server2.service.d
-sudo cp /vagrant/modules/hive_trunk/files/default.conf /etc/systemd/system/hive-metastore.service.d
 
 # Re-enable and start the new servers.
-sudo systemctl enable hive-metastore
+sudo systemctl enable hive2-metastore
 sudo systemctl enable hive2-server2
-sudo systemctl start hive-metastore
+sudo systemctl start hive2-metastore
 sudo systemctl start hive2-server2
