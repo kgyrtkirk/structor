a8400a0afbdcd0a6391a98ea0576d34476e548da
Carter Shanklin <carter@HW11972.local>
2017-09-19 11:00:13 -0700

    Fixed the host name command order

--- modules/hive_ssl/files/create_cert.sh
+++ modules/hive_ssl/files/create_cert.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+HOST=$(hostname)
 BASE_PATH=/etc
 KEYSTORE_PATH=$BASE_PATH/keystore.jks
 CERTIFICATE_PATH=$BASE_PATH/${HOST}.crt
 TRUSTSTORE_PATH=$BASE_PATH/truststore.jks
-TRUSTSTORE_PASS=abcdef
+TRUSTSTORE_PASS=fedcba
+KEY_PASS=abcdef
 cd /tmp
-HOST=$(hostname)
 echo $HOST > key_info.txt
 cat>>key_info.txt<<EOF
 Self
@@ -17,8 +18,8 @@ US
 yes
 EOF
 rm -f $KEYSTORE_PATH $CERTIFICATE_PATH $TRUSTSTORE_PATH
-keytool -genkey -alias $HOST -keyalg RSA -keystore $KEYSTORE_PATH -keysize 2048 -deststorepass $TRUSTSTORE_PASS -destkeypass $TRUSTSTORE_PASS < key_info.txt
-keytool -list -keystore /vagrant/modules/hive_ssl/files/keystore.jks -deststorepass $TRUSTSTORE_PASS
-keytool -export -alias $HOST -file $CERTIFICATE_PATH -keystore $KEYSTORE_PATH -srcstorepass $TRUSTSTORE_PASS -deststorepass $TRUSTSTORE_PASS
+keytool -genkey -alias $HOST -keyalg RSA -keystore $KEYSTORE_PATH -keysize 2048 -deststorepass $KEY_PASS -destkeypass $KEY_PASS < key_info.txt
+keytool -list -keystore $KEYSTORE_PATH -deststorepass $KEY_PASS
+keytool -export -alias $HOST -file $CERTIFICATE_PATH -keystore $KEYSTORE_PATH -srcstorepass $KEY_PASS -deststorepass $KEY_PASS
 echo yes | keytool -import -trustcacerts -alias $HOST -file $CERTIFICATE_PATH -keystore $TRUSTSTORE_PATH -srcstorepass $TRUSTSTORE_PASS -deststorepass $TRUSTSTORE_PASS
 keytool -list -keystore $TRUSTSTORE_PATH -deststorepass $TRUSTSTORE_PASS
